c82cd45009e48e514d57366298b4fd233f12b469
oriya_pelossof <pelossoforiya@gmail.com>
2024-12-02 23:30:40 +0200

    Basic Definitions

--- app/src/main/java/com/example/tictactoe/GameLogic.kt
+++ app/src/main/java/com/example/tictactoe/GameLogic.kt
@@ -1,4 +1,14 @@
 package com.example.tictactoe
 
 class GameLogic {
+
+    // x->1 O->2
+    private var currPlayer = 1
+
+    // A matrix that will hold the current state of the board
+    private var boardState = arrayOf(
+        intArrayOf(0, 0, 0),
+        intArrayOf(0, 0, 0),
+        intArrayOf(0, 0, 0)
+    )
 }
--- app/src/main/java/com/example/tictactoe/PlayActivity.kt
+++ app/src/main/java/com/example/tictactoe/PlayActivity.kt
@@ -1,12 +1,26 @@
 package com.example.tictactoe
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class PlayActivity : AppCompatActivity() {
+
+    //lateinit allows to initialize the variable later on in the project
+    private lateinit var gameLogic: GameLogic
+    private lateinit var box1: TextView
+    private lateinit var box2: TextView
+    private lateinit var box3: TextView
+    private lateinit var box4: TextView
+    private lateinit var box5: TextView
+    private lateinit var box6: TextView
+    private lateinit var box7: TextView
+    private lateinit var box8: TextView
+    private lateinit var box9: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +30,19 @@ class PlayActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        //Initialize the logic
+        gameLogic = GameLogic()
+
+        // Initialize TextView variables by finding them in the layout
+        box1 = findViewById(R.id.box1)
+        box2 = findViewById(R.id.box2)
+        box3 = findViewById(R.id.box3)
+        box4 = findViewById(R.id.box4)
+        box5 = findViewById(R.id.box5)
+        box6 = findViewById(R.id.box6)
+        box7 = findViewById(R.id.box7)
+        box8 = findViewById(R.id.box8)
+        box9 = findViewById(R.id.box9)
     }
 }
